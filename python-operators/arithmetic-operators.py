c8bcee540ef7c0687487a164e66eff94283dbf03
Vinod MV <vinodmv555@gmail.com>
2022-05-08 21:11:48 +0530

    first commit

---
+++ python-operators/arithmetic-operators.py
@@ -0,0 +1,16 @@
+x = 15
+y = 7
+# Add two opreands 
+print('x + y =', x + y)
+# Substract right operand from left 
+print('x - y =', x - y)
+#Multiply two operands
+print('x * y =', x * y)
+#Divide left operand by the right one 
+print('x / y =', x / y)
+#Floor division - division that results into whole number adjusted to the left in th number line. 
+print('x // y =', x // y)
+#Modulus - reaminder of the division of operand by the right. 
+print('x % y =', x % y)
+#Exponent - left operand raised to the power of right   
+print('x ** y =', x ** y)
---
+++ python-operators/comparison-operator.py
@@ -0,0 +1,12 @@
+x = 10
+y = 12 
+# Greater than 
+print('x > y =', x > y)
+# Less than 
+print('x < y =', x < y)
+# Equal to 
+print('x == y =', x == y)
+# Not equal to
+print('x != y =', x != y)
+# Greater than equal to
+print('x >= y =', x >= y)
