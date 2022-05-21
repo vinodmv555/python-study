64bdbce8b80d0ebc0dbbdc4bc374694678bba6f9
vinodmv555 <38179548+vinodmv555@users.noreply.github.com>
2022-05-21 09:49:41 +0530

    Delete if-loop.py

--- python-operators/if-loop.py
+++
@@ -1,9 +0,0 @@
-num = 3
-if num > 0:
-    print(num,"is a postive number")
-print("this is alaways printerd")
-
-num = -1
-if num > 0:
-    print(num, "is a postive number")
-print("this is also alays pritnerd")
